ac5895e093b75d25afff03194f6b1b3405f68d68
nath-d <azaazel0711@gmail.com>
2023-01-31 16:33:20 +0530

    login adding...

--- server/controller/UserController.js
+++ server/controller/UserController.js
@@ -1,11 +1,15 @@
 const pool = require("../database/conn");
+const bcrypt  = require('bcrypt');
+const JWT = require('jsonwebtoken');
+const ENV = require('../config.js');
+
 
 const getAllUser = async (req, res) => {
   let conn;
   try {
     conn = await pool.getConnection();
     console.log("db connected");
-    const sqlQuery = `SELECT * FROM test`;
+    const sqlQuery = `SELECT * FROM user`;
     let result = await pool.query(sqlQuery);
     res.json(result);
 
@@ -40,9 +44,10 @@ const addUser = async (req, res) => {
     conn = await pool.getConnection();
     console.log("db is active");
     let name = req.body.name;
+    let username = req.body.username;
     let email = req.body.email;
-    const sqlQuery = `INSERT INTO test (name, email) VALUES (?,?)`;
-    const result = await pool.query(sqlQuery, [name, email]);
+    const sqlQuery = `INSERT INTO user (name, email, username) VALUES (?,?,?)`;
+    const result = await pool.query(sqlQuery, [name, email, username]);
     console.log(result);
     res.json({userid : Number(result.insertId.toString())});
   } catch (err) {
@@ -52,4 +57,65 @@ const addUser = async (req, res) => {
   }
 };
 
-module.exports = { getAllUser, getSpecificUser, addUser };
+const login = async(req,res) =>{
+  let conn;
+  try {
+    let username = req.body.username;
+    let bodypassword = req.body.password;
+    let result;
+    conn = await pool.getConnection();
+    console.log("db is active");
+    const checkUser = new Promise( async (resolve, reject) => {
+    const sqlQuery = `SELECT * FROM user WHERE username=? `
+    result = await pool.query(sqlQuery, username);
+    res.json(result.username)
+    // console.log(passreturn)
+    // res.send(passreturn)
+    //const comparepass = bcrypt.compare(bodypassword, result.password)
+
+    resolve()
+    })
+    
+    // Promise.all([checkUser]).then(()=>{
+    //   console.log("result: "+Object.values(result))
+    //   const comparepass = bcrypt.compare(bodypassword, result.password).catch(error => {
+        
+    //   })
+    //   //   .then(passwordCheck => {
+    //   //     if(!passwordCheck) 
+    //   // return 
+    //   // res.status(400).send({error:"Password doesnt EXIST"})
+    //   //   })
+    //   res.send(comparepass)
+    // })
+    
+    
+    // bcrypt.compare(password, result.password)
+    // .then(passwordCheck => {
+      
+
+    //   //Create JWT Token
+    //   const token = JWT.sign({
+    //     userid: result._id,
+    //     username: result.username,
+    //   }, 'secret', {expiresIn:"24h"});
+    //   return res.status(200).send({
+    //     msg: "Login Successful...",
+    //     username:result.username,
+    //     token
+    //   })
+
+    // })
+    // .catch(error => {
+    //   return res.status(400).send({error: "Password doesnt match"})
+    // })
+    // console.log(result);
+    // res.json({userid : Number(result.insertId.toString())});
+  } catch (err) {
+    throw err;
+  } finally {
+    if (conn) return conn.release();
+  }
+}
+
+module.exports = { getAllUser, getSpecificUser, addUser, login };
--- server/router/user.js
+++ server/router/user.js
@@ -3,18 +3,23 @@ const {
   getAllUser,
   getSpecificUser,
   addUser,
+  login,
 } = require("../controller/UserController");
 
 const userRouter = express.Router();
 
 //Get method
+userRouter.get('/', (req, res)=>{
+  res.send('api route')
+})
 userRouter.get("/user", getAllUser);
 userRouter.get("/user/:id", getSpecificUser);
 
 //Put method
 
 //Post method
-userRouter.route('/login').post()
+userRouter.post('/login', login);
+
 
 
 //delete method
